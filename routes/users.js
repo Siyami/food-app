e85704828654a29b1decad3b3801f2f1a5c63c6f
Siyami Avci <siyamiavci@gmail.com>
2017-02-21 21:09:06 -0800

    Add POST /users middleware

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,29 @@
 'use strict';
 
+const bcrypt = require('bcrypt-as-promised');
 const express = require('express');
+const knex = require('../knex');
+
 const router = express.Router();
 
-module.exports = router;
+router.post('/users', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12)
+    .then((hashed_password) => {
+      return knex('users').insert({
+        email: req.body.email,
+        hashed_password: hashed_password
+      }, '*');
+    })
+    .then((users) => {
+      const user = users[0];
 
-// Add more stuff
+      delete user.hashed_password;
+
+      res.send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
