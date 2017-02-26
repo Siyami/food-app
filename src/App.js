28af2c2ee39983fe16fb16bf41ef85d8a9f7e96e
Siyami Avci <siyamiavci@gmail.com>
2017-02-25 16:19:43 -0800

    Close #4 Add buttons on Main Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Navbar, Button, Nav, NavItem, Grid, Row, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello dsd</h1>
+          <Navbar className="Navbar" inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#">Food App</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">Link</NavItem>
+                <NavItem eventKey={2} href="#">Link</NavItem>
+              </Nav>
+              <Nav pullRight>
+                <NavItem eventKey={1} href="#">Log In</NavItem>
+                <NavItem eventKey={2} href="#">Sign Up</NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Grid>
+            <Row>
+              <Col xs={6} xsOffset={5}>
+                <Button bsStyle="primary">Search Meal</Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} xsOffset={5}>
+                <Button bsStyle="primary">Search Exercise</Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} xsOffset={5}>
+                <Button bsStyle="primary">View Details</Button>
+              </Col>
+            </Row>
+          </Grid>
+
       </div>
     );
   }
