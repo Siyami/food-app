fd14105fb7b6ab3f328a6de21f5e850980e22a64
Siyami Avci <siyamiavci@gmail.com>
2017-02-26 16:39:44 -0800

    Close #8 Display data in SearchMeal component

--- src/components/SearchMeal.js
+++ src/components/SearchMeal.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Table, Grid, Row, Col, Thumbnail, Button } from 'react-bootstrap';
+import axios from 'axios';
 
 class SearchMeal extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchMeal: ''
+      searchMeal: '',
+      photo: '',
+      foodName: '',
+      servingUnit: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -15,7 +19,6 @@ class SearchMeal extends Component {
 
   handleChange(event) {
     const nextState = {
-      // we do this because we have different names if you don't have it we need if()...
       [event.target.name]: event.target.value
     };
 
@@ -24,13 +27,36 @@ class SearchMeal extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    
+    axios({
+      method: 'post',
+      url: 'https://trackapi.nutritionix.com//v2/natural/nutrients',
+      data: {
+        "query": this.state.searchMeal,
+        "timezone": "US/Eastern"
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'x-app-id': '816ac43f',
+        'x-app-key': '15dda15a013d5b7d3ded1167c25c2893',
+        'x-remote-user-id': 0
+      }
+    })
+    .then(({data}) => {
+      console.log(data.foods);
+      this.setState({
+        photo: data.foods[0].photo.highres,
+        foodName: data.foods[0].food_name,
+        servingUnit: data.foods[0].serving_unit,
+        servingQuantity: data.foods[0].serving_qty
+      })
 
-    // Perhaps send a JSON string to a server...
-    JSON.stringify({
-      searchMeal: this.state.searchMeal
+    })
+    .catch((err) => {
+      console.log(err);
     });
 
-    // Reset the form
+    //Reset the form
     this.setState({
       searchMeal: ''
     });
@@ -38,26 +64,85 @@ class SearchMeal extends Component {
 
   render() {
     return (
-      <Grid>
-        <Row>
-          <Col xs={6} xsOffset={4}>
-           <form onSubmit={this.handleSubmit}>
-            <label>
-              <input
-                name="searchMeal"
-                onChange={this.handleChange}
-                type="text"
-                value={this.state.searchMeal}
-              />
-            </label>
+      <div>
+        <Grid>
+          <Row>
+            <Col xs={6} xsOffset={4}>
+              <form onSubmit={this.handleSubmit}>
+                <label>
+                  <input
+                    name="searchMeal"
+                    onChange={this.handleChange}
+                    type="text"
+                    value={this.state.searchMeal}
+                  />
+                </label>
+                <input type="submit" value="Search Meal" />
+              </form>
+            </Col>
+          </Row>
+        </Grid>
 
-            <input type="submit" value="Search Meal" />
-          </form>
+        <div>
+          {
+            (this.state.photo)
+            ? (<Grid>
+              <Row>
+                <Col xs={6} md={6} >
+                  <h2>Meal Details</h2>
+                  <Thumbnail src={this.state.photo} alt="242x200">
+                    <h3>{this.state.foodName}</h3>
+                    <p>{`Serving Unit: ${this.state.servingUnit}`}</p>
+                    <p>{`Serving Quantity: ${this.state.servingQuantity}`}</p>
+                    <p>
+                      <Button bsStyle="primary">Button</Button>&nbsp;
+                      <Button bsStyle="default">Button</Button>
+                    </p>
+                  </Thumbnail>
+                </Col>
 
-          </Col>
-        </Row>
+                <Col xs={6} md={6} >
+                  <h2>Nutritional Data</h2>
+                  <Table striped bordered condensed hover>
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </Col>
+              </Row>
+            </Grid>)
 
-      </Grid>
+            : (<Grid>
+                <Row>
+                  <Col xs={6} md={5} xsOffset={4}>
+                    <h4>Please Search a Meal</h4>
+                  </Col>
+
+                </Row>
+              </Grid>)
+          }
+        </div>
+
+      </div>
     )
   }
 }
